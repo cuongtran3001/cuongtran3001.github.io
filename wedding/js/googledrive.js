5953ef81e6e5ca1fd924d095a0ee18e297262917
ctran <cuong.tran@sutrixmedia.com>
2015-11-05 10:39:00 +0700

    gd: authenticate

--- wedding/js/googledrive.js
+++ wedding/js/googledrive.js
@@ -5,17 +5,42 @@ function onGooglClientApiLoadedHandler() {
 }
 
 function GoogleDrive() {
-
+	this.CLIENT_ID = '691726389794-4boot0bt98elrqjr6rqos45dvs7bf0f7.apps.googleusercontent.com';
+    this.API_KEY = 'AIzaSyCbJYq6i_CJzt8okRFQw4frnBtvjp4IV80';
+    this.SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
 }
 
 GoogleDrive.prototype.init = function() {
-	console.log('init');
+	gapi.client.setApiKey(API_KEY);
 };
 
 GoogleDrive.prototype.connect = function() {
-	console.log('connect');
+	gapi.auth.authorize({
+        'client_id': this.CLIENT_ID,
+        'scope': this.SCOPES.join(' '),
+        'immediate': true
+    }, this.onAuthResultHandler);
 };
+
+GoogleDrive.prototype.onAuthResultHandler = function(authResult) {
+    if (authResult && !authResult.error) {
+		this.loadDriveApi();
+    }
+
+	else {
+		
+	}
+};
+
+GoogleDrive.prototype.loadDriveApi = function() {
+	gapi.client.load('drive', 'v2', onDriveLoadHandler);
+};
+
+GoogleDrive.prototype.onDriveLoadHandler = function() {
+	console.log(gapi.client.drive.files.list);
+};
+
 
 $('#googledrive-connect').on('click', function(evt) {
 	googleDrive.connect();
-}
+});
