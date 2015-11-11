26e2f4ea7b191ac9b40d9aa8daa06551733e1f28
ctran <cuong.tran@sutrixmedia.com>
2015-11-11 14:02:48 +0700

    call me

--- wedding/js/FaceBook.js
+++ wedding/js/FaceBook.js
@@ -1,7 +1,5 @@
 function FaceBook() {
-	this.CLIENT_ID = '131211991594-2l4cskeuaoa2o5m0jqdh0p79uksishnl.apps.googleusercontent.com';
-    this.API_KEY = 'AIzaSyDm_hXiAoCjmg01rTc6aPSuMcZWOF8EgqU';
-    this.SCOPES = ['https://www.googleapis.com/auth/drive'];
+	this.SCOPES = 'public_profile, user_photos, user_videos';
 	
 	this.state = null;
 }
@@ -19,17 +17,18 @@ FaceBook.prototype.init = function() {
 FaceBook.prototype.connect = function() {
 	var	that = this;
 	
+	$('#cloud-breadcrumb').find('.separate').hide();
+	$('#cloud-breadcrumb').find('.album').html('');
+				
 	if (this.state) {
+		that.callMe();
 		that.callAPI('me/albums?fields=id,name,cover_photo', function(response) { that.onAlbumLoadedHandler(response) });
 	} else {	
 		FB.login(function(response) {
 			if (response.status === 'connected') {
 				that.state = 'LOGIN';
-				
-				$('#cloud-breadcrumb').find('.separate').hide();
-				$('#cloud-breadcrumb').find('.album').html('');
-		
-				this.callAPI('me/albums?fields=id,name,cover_photo', function(response) { that.onAlbumLoadedHandler(response) });
+				that.callMe();
+				that.callAPI('me/albums?fields=id,name,cover_photo', function(response) { that.onAlbumLoadedHandler(response) });
 			}
 		  
 			else if (response.status === 'not_authorized') {
@@ -40,37 +39,9 @@ FaceBook.prototype.connect = function() {
 				// The person is not logged into Facebook, so we're not sure if
 				// they are logged into this app or not.
 			}
-		}, {scope: 'public_profile, user_photos, user_videos'});
+		}, {scope: this.SCOPES});
 	}
-
-/*
-	var that = this;
-	gapi.auth.authorize({
-        'client_id': this.CLIENT_ID,
-        'scope': this.SCOPES.join(' '),
-        'immediate': true
-    }, function(authResult) {
-		if (authResult && !authResult.error) {
-			that.loadDriveApi();
-		}
-	});
-	
-	//dispatch event here
-	$('#googledrive-loadding').show();
-	$('#googledrive-content').empty();
-	$('#googledrive-breadcrumb').hide();
-	$('#uploadGoogle').modal('show');
-	
-	var that = this;
-	setTimeout(function() {
-		$('#googledrive-loadding').hide();
-		$('#googledrive-breadcrumb').show();
-	}, 2000);
-	*/
 };
-
-//{album-id}/photos
-//me/albums?fields=id,name,cover_photo
 
 FaceBook.prototype.callAPI = function(apiQuery, callback) {
 	
@@ -88,6 +59,17 @@ FaceBook.prototype.callAPI = function(apiQuery, callback) {
 				$('#cloud-loadding').hide();
 				$('#cloud-breadcrumb').show();
 				callback(response);
+			}
+		}
+	);
+};
+
+FaceBook.prototype.callMe = function() {
+	FB.api(
+		'me/',
+		function (response) {
+			if (response && !response.error) {					
+				$('#cloud-breadcrumb').find('.root').html(response.name);
 			}
 		}
 	);
