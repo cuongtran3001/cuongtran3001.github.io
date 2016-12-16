a726de9495e6530d5a9c7067710ec07f60f93c74
ctran <cuong.tran@sutrixmedia.com>
2016-12-16 16:59:48 +0700

    update fv

--- facevui/js/script.js
+++ facevui/js/script.js
@@ -1,4 +1,77 @@
 
+;(function($, window, FB, undefined) {
+  'use strict';
+
+  var pluginName = 'facebook-social';
+
+  var showHideButton = function(isConnected) {
+    if (isConnected) {
+      this.vars.btnShare.show();
+      this.vars.btnLogin.hide();
+      this.vars.status.hide();
+    }
+    else {
+      this.vars.status.show();
+      this.vars.btnLogin.show();
+      this.vars.btnShare.hide();
+    }
+  };
+  var checkLoginState = function() {
+    var that = this;
+    FB.getLoginStatus(function(response) {
+      showHideButton.call(that, response.status === 'connected');
+    });
+  };
+
+  function Plugin(element, options) {
+    this.element = $(element);
+    this.options = $.extend({}, $.fn[pluginName].defaults, this.element.data(), options);
+    this.init();
+  }
+
+  Plugin.prototype = {
+    init: function() {
+      var that = this;
+
+      that.vars = {
+        btnLogin: that.element.find('.btn-login'),
+        btnShare: that.element.find('.btn-share'),
+        status: that.element.find('.status')
+      };
+
+      checkLoginState.call(this);
+
+      that.vars.btnLogin.on('click', function() {
+        FB.login(function() {
+          checkLoginState.call(that);
+        }, {scope: 'public_profile,email'});
+      });
+
+    },
+    destroy: function() {
+      $.removeData(this.element[0], pluginName);
+    }
+  };
+
+  $.fn[pluginName] = function(options, params) {
+    return this.each(function() {
+      var instance = $.data(this, pluginName);
+      if (!instance) {
+        $.data(this, pluginName, new Plugin(this, options));
+      } else if (instance[options]) {
+        instance[options](params);
+      }
+    });
+  };
+
+  $.fn[pluginName].defaults = {};
+
+  $(function() {
+    $('[data-' + pluginName + ']')[pluginName]();
+  });
+
+}(jQuery, window, window.FB));
+
 ;(function($, window, undefined) {
   'use strict';
 
@@ -14,18 +87,31 @@
     init: function() {
       var that = this,
           scrollTop,
-          heightWin = $(window).height(),
-          heightItem = 0,
-          heightDoc = $(document).height();
+          scrollPosition,
+          heightDoc,
+          heightWin,
+          limit,
+          offset,
+          totalRecord;
 
-      console.log(heightItem);
+      that.elmLoading = that.element.next('.icon-circle');
       that.isCalling = false;
+      that.isLoading = true;
+      totalRecord = that.options.totalRecord || 0;
+      limit = that.options.request.limit || 0;
+      offset = that.options.request.offset || 0;
 
       $(window).scroll(function() {
+        heightDoc = $(document).height();
+        heightWin = $(window).height();
         scrollTop = $(window).scrollTop();
-        if (scrollTop + heightItem >= heightDoc - heightWin) {
-          that.loadFeatured();
-          heightItem = that.element.find('.thumbnail-wrapper').height();
+        scrollPosition = heightWin + scrollTop;
+        if ( (heightDoc - scrollPosition) / heightDoc === 0 ) {
+          if ( that.isLoading ) {
+            that.elmLoading.show();
+            setTimeout(function(){ that.loadFeatured(); }, 1000);
+
+          }
         }
       });
     },
@@ -47,10 +133,15 @@
         },
         success: function(res) {
           if ('success' === res.status) {
-            console.log(res.status);
-            that.element.append(res.data);
+            that.elmLoading.hide();
+            that.element.append(res.data[dataRequest.offset]);
 
-            dataRequest.offset += dataRequest.limit;
+            dataRequest.offset = dataRequest.offset + dataRequest.limit;
+            that.element.data('request', dataRequest);
+
+            if ( dataRequest.offset === res.totalRecord ) {
+              that.isLoading = false;
+            }
           }
         },
         error: function(xhr, status, error) {
@@ -86,3 +177,58 @@
 
 }(jQuery, window));
 
+
+;(function($, window, undefined) {
+  'use strict';
+
+  var pluginName = 'scroll-header';
+
+  function Plugin(element, options) {
+    this.element = $(element);
+    this.options = $.extend({}, $.fn[pluginName].defaults, this.element.data(), options);
+    this.init();
+  }
+
+  Plugin.prototype = {
+    init: function() {
+      var that = this,
+          scrollTop,
+          isScrollPos = 0;
+
+      $(window).off('scroll.' + pluginName).on('scroll.' + pluginName, function() {
+        scrollTop = $(window).scrollTop();
+
+        if((scrollTop > isScrollPos) || (scrollTop === 0)) {
+          that.element.removeClass('scroll-up');
+        }
+        else {
+          that.element.addClass('scroll-up');
+        }
+
+        isScrollPos = scrollTop;
+      }).trigger('scroll.' + pluginName);
+    },
+    destroy: function() {
+
+      $.removeData(this.element[0], pluginName);
+    }
+  };
+
+  $.fn[pluginName] = function(options, params) {
+    return this.each(function() {
+      var instance = $.data(this, pluginName);
+      if (!instance) {
+        $.data(this, pluginName, new Plugin(this, options));
+      } else if (instance[options]) {
+        instance[options](params);
+      }
+    });
+  };
+
+  $.fn[pluginName].defaults = {};
+
+  $(function() {
+    $('[data-' + pluginName + ']')[pluginName]();
+  });
+
+}(jQuery, window));
